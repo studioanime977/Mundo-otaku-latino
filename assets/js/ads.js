c47c36a744820f08176afbaa5d023fe66aefaf50
studioanime977 <studiootaku6@gmail.com>
2025-11-18 17:06:00 -0500

    git push origin main

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -23,11 +23,25 @@
   const s2 = '//pl28083609.effectivegatecpm.com/ab/ec/46/abec46d039b00848567c45e1c5552b98.js';
   const i1 = 'https://www.effectivegatecpm.com/v27wbcqe?key=7d1586d18ef6eda218f888b9468a6a80';
   const i2 = 'https://www.effectivegatecpm.com/g71km9rd?key=3dd0d64887dc4e4a06a9e86fa43bf371';
+  const a1 = '//aggressivestruggle.com/b/X/VSstd.GKlT0DY-W/cX/qeemK9zusZRU/lCkpPmTlYb2DNizYUT1/NhzGEft/NcjEYX3tNKTBUy3bMcgN';
+  const a2 = '//aggressivestruggle.com/b.XJVusldVGblt0-Y_WJcp/Ue/mt9JuIZGU/lWkKP/T/YY2ANnzXYT0lNRDFIqtUNzjqY/3/NkjSQj0UMUwR';
+  const i3 = 'https://impeccable-sense.com/2lT0ce';
+  function appendAggressive(src) {
+    const s = document.createElement('script');
+    s.src = src;
+    s.async = true;
+    s.referrerPolicy = 'no-referrer-when-downgrade';
+    const l = document.scripts[document.scripts.length - 1];
+    if (l && l.parentNode) l.parentNode.insertBefore(s, l); else document.head.appendChild(s);
+  }
   function init() {
     appendScript(s1);
     appendScript(s2);
     addIframe(i1, 100);
     addIframe(i2, 100);
+    appendAggressive(a1);
+    appendAggressive(a2);
+    addIframe(i3, 100);
   }
   if (document.readyState === 'loading') document.addEventListener('DOMContentLoaded', init);
   else init();
